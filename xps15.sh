8c225ec1b45040e31af550b78b758d0013d7775c
Rob Hofmann <rob.hofmann@outlook.com>
2022-03-23 14:13:13 +0100

    Update xps15.sh

--- xps15.sh
+++ xps15.sh
@@ -1,9 +1,9 @@
-export DEVICE=/dev/nvme1n1
+export DEVICE=/dev/nvme0n1
 export BOOT_PARTITION=${DEVICE}p1
 export SWAP_PARTITION=${DEVICE}p2
 export ROOT_PARTITION=${DEVICE}p3
-export SWAP_SIZE=32G
-export HOSTNAME=michielxps15
+export SWAP_SIZE=16G
+export HOSTNAME=robxps15
 export EXTRA_PACKAGES="networkmanager"
 export MICROCODE_PKG="intel-ucode"
 export EXTRA_MODULES="i915"
